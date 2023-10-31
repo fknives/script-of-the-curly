e107e6956f83ea4ed836e6ba7d889547ea35b6f7
Gergely Hegedis <hegedusgergely@yahoo.com>
2023-10-31 15:21:11 +0200

    ssh-keygen

---
+++ make-new-key.sh
@@ -0,0 +1,4 @@
+# in order to use multiple github accounts, you have to add the username to the url for example: git@github.com-myusername:...
+
+# generate a new rsa key, alternative: ssh-keygen  -t ecdsa -b 521
+ssh-keygen -t rsa -b 4096
